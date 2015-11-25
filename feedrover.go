4f42394be37b3dc7fa004c3562b783e8f030b420
akim <akim_delli@hotmail.com>
2015-11-24 23:50:21 -0500

    postgres integration

--- feedrover.go
+++ feedrover.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"fmt";
+	"fmt"
 	"net/http"
 	"io/ioutil"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
 )
+
+type StringSlice []string
 
 func main() {
 	resp, err := http.Get("http://hipsterjesus.com/api")
@@ -12,9 +17,41 @@ func main() {
 		fmt.Println(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("response Body:", string(body))
+	//fmt.Println("response Body:", string(body))
+
+	db, err := sql.Open("postgres", "user=feedrover host=/var/run/postgresql dbname=feedrover password=feedrover sslmode=disable")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	var (
+		equip_id int
+		color string
+		)
+	rows, err := db.Query("select equip_id, color from playground")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&equip_id, &color)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(equip_id, color)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
 }
+
+
+
